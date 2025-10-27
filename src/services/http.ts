819a0acefd39de2fd0c6cf8438c351ca935d682e
Ridwan Arifandi <orangerdigiart@gmail.com>
2025-10-27 19:33:09 +0700

    set ttl cache

--- src/services/http.ts
+++ src/services/http.ts
@@ -3,11 +3,12 @@ import { setupCache } from 'axios-cache-interceptor'
 
 // Cache TTL: 1 hour
 const ONE_HOUR_MS = 60 * 60 * 1000
+const ONE_MINUTE_MS = 60 * 1000
 
 // Create a shared axios instance with in-memory cache.
 // The cache key considers method + url + params by default.
 export const http = setupCache(axios.create(), {
-  ttl: ONE_HOUR_MS,
+  ttl: ONE_MINUTE_MS,
   interpretHeader: false, // ignore cache headers; rely on ttl
   methods: ['get'], // only cache GET
 })
